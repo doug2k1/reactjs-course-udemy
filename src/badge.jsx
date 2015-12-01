76c57687259ba2d6afb21eea1d3512bc4ff6a3aa
Douglas Matoso <doug2k1@gmail.com>
2015-11-30 23:17:09 -0200

    States and events.

--- src/badge.jsx
+++ src/badge.jsx
@@ -1,10 +1,20 @@
 var React = require('react');
 
 module.exports = React.createClass({
+    getInitialState: function () {
+        return { count: this.props.number };
+    },
+
+    handleClick: function () {
+        this.setState({
+            count: this.state.count + 1
+        });
+    },
+
     render: function () {
         return (
-            <button className="btn btn-primary" type="button">
-                {this.props.children} <span className="badge">{this.props.number}</span>
+            <button className="btn btn-primary" type="button" onClick={this.handleClick}>
+                {this.props.children} <span className="badge">{this.state.count}</span>
             </button>
         );
     }
